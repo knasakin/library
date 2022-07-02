c503e7269f6c0f3e48c93076dcb056d64d474b3a
kostik <kostikvlz@mail.ru>
2022-07-02 19:46:18 +0300

    renamed project to library

--- libproject/libproject/settings.py
+++ libproject/libproject/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = environ.get('LIBRARY_KEY')
+SECRET_KEY = environ.get('LIBRARY_DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles'
-    'Library',
+    'django.contrib.staticfiles',
+    'library'
 ]
 
 MIDDLEWARE = [
